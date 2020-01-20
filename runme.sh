605727d6c5fd34e4f7b8161cb218b5ef58ca6fe9
Ubuntu <ubuntu@ip-172-31-36-225.us-east-2.compute.internal>
2020-01-20 16:37:40 +0000

    adding for failsafe password

---
+++ runme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#!/bin/sh
+source /home/ubuntu/mk_the_yogi_questionmark_ihatespecialcharacters/venv/bin/activate
+
+python /home/ubuntu/mk_the_yogi_questionmark_ihatespecialcharacters/email_alert_cron_test.py
+
+ 
